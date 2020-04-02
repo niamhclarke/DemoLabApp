f9330f3e4d92f44a2575562d199821090100de06
Niamh Clarke <niamh.clarke@pwc.com>
2020-04-02 15:09:17 +0100

    added 'About' functionality

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,6 +11,18 @@ namespace DemoLabApp.Controllers
 {
     public class HomeController : Controller
     {
+
+        #region some code before...
+        public IActionResult Privacy()
+        {
+            return View()
+        }
+        public IActionResult About()
+        {
+            return View()
+        }
+        #region more code after...
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
